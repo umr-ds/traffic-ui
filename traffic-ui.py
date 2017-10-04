746c6cb4cb728fb771954147df1d81317f620565
geistesk <github.jyf300hj@0x21.biz>
2017-10-04 23:54:09 +0200

    CRC32-hash for flows and cache plots
    The flow-class got a hash-field which is based on a CRC32 over the pcap-data.
    This hash is used to name the cached plots. It also shows the difference
    between two different pcaps with the same name.

--- traffic-ui.py
+++ traffic-ui.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from bottle import abort, response, route, run
+from bottle import abort, route, run, static_file
 from flowinspector import flow
 from io import BytesIO
 from os import listdir, path
 
 pcap_path = './input'
+img_cache_path = './cache'
 
 @route('/')
 def index():
@@ -17,42 +18,41 @@ def index():
 
 @route('/show/<filename>')
 def show_pcap(filename):
-    fullpath = check_fetch_fullpath(filename)
-    req_flow = flow(fullpath)
-    req_flow.lookupASN()
-    return '<img src="/plot/{0}" /><pre>{0}\n\n{1}</pre>'.format(filename, repr(req_flow))
+    req_flow = flow_from_filename(filename)
+    return '<img src="/plot/{0}.png" /><pre>{0}\n\n{1}</pre>'.format(filename, repr(req_flow))
 
-@route('/plot/<filename>')
+@route('/plot/<filename>.png')
 def plot_pcap(filename):
-    import matplotlib
-    matplotlib.use('Agg')
+    req_flow = flow_from_filename(filename)
+    img_name = req_flow.hash + '.png'
+    plot_path = img_cache_path + '/' + img_name
 
-    import matplotlib.pyplot as plt
-    plt.rcParams['figure.figsize'] = (9, 7)
+    if not path.isfile(plot_path):
+        import matplotlib
+        matplotlib.use('Agg')
 
-    fullpath = check_fetch_fullpath(filename)
-    req_flow = flow(fullpath)
-    req_flow.lookupASN()
-    plt = req_flow.show(show=False)
+        import matplotlib.pyplot as plt
+        plt.rcParams['figure.figsize'] = (9, 7)
 
-    imgdata = BytesIO()
-    plt.savefig(imgdata, format='png')
-    imgdata.seek(0)
+        plot = req_flow.show(show=False)
+        plot.savefig(plot_path, format='png')
+        plot.close()
 
-    plt.close()
+    return static_file(img_name, root=img_cache_path)
 
-    response.content_type = 'image/png'
-    return imgdata
-
-def check_fetch_fullpath(filename):
+def flow_from_filename(filename):
     fullpath = pcap_path + '/' + filename
+
     if not path.isfile(fullpath):
         abort(404, 'No such file')
     if not fullpath.endswith('.pcap'):
         abort(403, 'Not allowed')
     if path.dirname(fullpath) != pcap_path:
         abort(403, 'Not allowed')
-    return fullpath
+
+    req_flow = flow(fullpath)
+    req_flow.lookupASN()
+    return req_flow
 
 
 if __name__ == "__main__":
